89cee499eface3eef51b6fd90d2a27821b846f6d
demoInt <lovishsinghwork@gmail.com>
2023-02-16 15:50:42 +0530

    Uploading raw project files

---
+++ serverSide/projectPool/src/test/java/com/example/projectPool/ProjectPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectPool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
